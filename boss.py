352abb13313bf724bd1ea8911427e8dd069d8948
var-mixer <x1145@foxmail.com>
2020-12-14 12:25:50 +0800

    适配国服三阶段

--- boss.py
+++ boss.py
@@ -1,42 +1,57 @@
-# 此部分来自艾琳佬，感谢艾琳佬，赞美艾琳佬
 import math
 
 bossData = {
     'scoreRate': [
         [1, 1, 1.3, 1.3, 1.5],
         [1.3, 1.3, 1.8, 1.8, 2],
+        [2.0, 2.0, 2.5, 2.5, 3.0]
     ],
-    'hp': [6000000, 8000000, 10000000, 12000000, 20000000],
-    # 国服无多周目差别
-    'max': 2,
+    'hp': [
+        [6000000, 8000000, 10000000, 12000000, 20000000],
+        [6000000, 8000000, 10000000, 12000000, 20000000],
+        [6000000, 8000000, 10000000, 12000000, 20000000]
+    ],
+    # 标记每个阶段的最大周目数（累计周目）
+    "cy": [1, 6]
 }
 
-def calc_hp(hp_base: int):
-    zm = 1
-    king = 1
-    cc = 0.0
-    remain = 0.0
-    damage = 0
-    remainHp = 0.0
-    remainPer = 0.0
+def calc_hp(score: int):
+    # 输出时应当全部加1
+    king = 0
+    cycle = 0
+    stage = 0
+    max_stage = len(bossData["cy"])
+    score_left = score
 
-    while True:
-        nowZm = bossData['max'] - 1 if zm > bossData['max'] else zm - 1
-        cc += bossData['scoreRate'][nowZm][king - 1] * bossData['hp'][king - 1]
-        if cc > hp_base:
-            cc -= bossData['scoreRate'][nowZm][king - 1] * bossData['hp'][king - 1]
-            remain = (hp_base - cc) / bossData['scoreRate'][nowZm][king - 1]
-            damage += remain
-            remainPer = 1.0 - remain / bossData['hp'][king - 1]
-            remainHp = bossData['hp'][king - 1] - remain
-            break
-        damage += bossData['hp'][king - 1]
-        if king == 5:
-            zm += 1
-            king = 1
-            continue
-        king += 1
-    remainPer *= 100
-    bdk = bossData['hp'][king - 1]
-    return f'{zm}周目{king}王 [{math.floor(remainHp)}/{bdk}]  {round(remainPer, 2)}%'
+    hp_max = bossData["hp"][stage][king]
+    rate = bossData["scoreRate"][stage][king]
+    # 当剩余分数大于当前BOSS分数时循环
+    while score_left > hp_max*rate:
+
+
+        # 获取当前BOSS的倍率和最大血量
+        rate = bossData["scoreRate"][stage][king]
+        hp_max = bossData["hp"][stage][king]
+
+        # 计算剩余血量
+        score_left = score_left - rate * hp_max
+
+        # 修改BOSS编号，检查是否进入下一周目
+        if king >=  4:
+            king = 0
+            cycle = cycle + 1
+        else:
+            king = king + 1
+        # 检查周目数，是否进入下一阶段
+        if stage < max_stage:
+            if  cycle > bossData["cy"][stage] - 1:
+                stage = stage + 1
+
+        rate = bossData["scoreRate"][stage][king]
+        hp_max = bossData["hp"][stage][king]
+    
+    # 最后剩余分数按照比率转化为血量
+
+    hp_left = hp_max - score_left / rate
+    return f'{cycle+1}周目{king+1}王 [{math.floor(hp_left)}/{hp_max}]'
 
